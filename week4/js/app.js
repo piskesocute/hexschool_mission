290c371f2a4ecdac79286d95f4077f5b486a97a4
Evan <happyday9837@gmail.com>
2022-02-22 19:39:48 +0800

    week 4

---
+++ week4/js/app.js
@@ -0,0 +1,28 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+const app = createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      },
+    };
+  },
+  methods: {
+    login() {
+      const apiUrl = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios.post(apiUrl, this.user)
+        .then(res => {
+          const { token, expired } = res.data
+          document.cookie = `backstageCookie=${token}; expires=${new Date(expired)}; path=/`;
+          window.location.href = './backstage.html'
+          console.log(token);
+        }).catch(err => {
+          alert(err.response.data.message + '\r' + err.response.data.error.message)
+          window.location.reload();
+        })
+    }
+  },
+})
+app.mount('#app')
---
+++ week4/js/product.js
@@ -0,0 +1,201 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+let selectModal = "";
+let deleteModal = "";
+let successModal = "";
+let errorModal = '';
+let deletebatchModal = '';
+
+const app = createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      path: 'evan-classuse',
+      productsList: [],
+      itemList: {
+        imagesUrl: [],
+      },
+      is_Edit: null,
+      deleteBatchArr: [],
+      deletebatcherrdata:[],
+    }
+  },
+  methods: {
+
+    //modal區
+    // 開啟新增商品modal按鈕
+    addNewData() {
+      this.is_Edit = 0;
+      this.itemList = {
+        imagesUrl: ['']
+      };
+      selectModal.show();
+    },
+    // 開啟查看或編輯modal按鈕
+    checkItem(item) {
+      this.is_Edit = 1;
+      this.itemList = JSON.parse(JSON.stringify(item));
+      // 判斷商品數量，售完則將狀態改為2
+      if (this.itemList.quantity < 1) this.itemList.is_enabled = 2;
+      if (!this.itemList.imagesUrl) {
+        this.itemList.imagesUrl = [''];
+      }
+      selectModal.show()
+    },
+    // 開啟刪除modal
+    openDeleteModal(item) {
+      this.is_Edit = 2;
+      this.itemList = item
+      deleteModal.show()
+    },
+    openDeletebatchModal() {
+      this.is_Edit = 3;
+      deletebatchModal.show();
+    },
+    // 開啟操作成功Modal
+    openSuccessModal() {
+      successModal.show();
+    },
+    // 開啟操作失敗Modal
+    openErrorModal() {
+      errorModal.show();
+    },
+    // 關閉增刪查找modal
+    closeModal() {
+      
+      if (this.is_Edit <= 1) {
+        selectModal.hide();
+      } else if (this.is_Edit === 2) {
+        deleteModal.hide();
+      } else if (this.is_Edit === 3) {
+        deletebatchModal.hide();
+      } else if (this.is_Edit === 4) {
+        deletebatchModal.hide();
+      }
+    },
+    // 關閉操作成功
+    closeSuccessModal() {
+      successModal.hide();
+      this.getProductList();
+      
+    },
+    // 關閉操作失敗
+    closeErrorModal() {
+      errorModal.hide();
+      this.getProductList();
+    },
+    // api操作
+    // push & put api
+    pushData(id) {
+      let url = `${this.apiUrl}/api/${this.path}/admin/product`
+      let methods = 'post'
+      if (this.is_Edit === 1) {
+        url = `${this.apiUrl}/api/${this.path}/admin/product/${id}`
+        methods = 'put'
+      }
+      axios[methods](url, { data: this.itemList })
+        .then((res) => {
+          this.closeModal();
+          this.openSuccessModal();
+        }).catch((err) => {
+          this.closeModal();
+          this.openErrorModal();
+        })
+    },
+    //刪除deleteApi
+    deleteData(id) {
+      let url = `${this.apiUrl}/api/${this.path}/admin/product/${id}`
+      axios.delete(url)
+        .then((res) => {
+          this.closeModal();
+          this.openSuccessModal()
+        }).catch((err) => {
+          this.closeModal();
+          this.openErrorModal();
+        })
+    },
+    // 問題
+    //批量刪除
+    async deleteBatchData() {
+      let deleteFinish = 0
+      for(let id of this.deleteBatchArr){
+        let url = `${this.apiUrl}/api/${this.path}/admin/product/${id}`;
+        try{
+          await axios.delete(url);
+          deleteFinish++;
+        }catch(err){
+          this.deletebatcherrdata.push(`${id}`)
+        }
+      }
+      this.closeModal();
+      if (deleteFinish === this.deleteBatchArr.length) {
+        this.openSuccessModal();
+      } else {
+        this.openErrorModal();
+      }
+    },
+    // 將checkbox 選取的商品送入刪除序列
+    checkItemInDeleteArr(item) {
+      let isInArr = this.deleteBatchArr.indexOf(item)
+      if (isInArr < 0) {
+        this.deleteBatchArr.push(item)
+      } else {
+        this.deleteBatchArr.splice(isInArr, 1)
+      }
+    },
+    //登入驗證
+    checkLogin() {
+      const url = `${this.apiUrl}/api/user/check`
+      axios.post(url)
+        .then((res) => {
+          this.getProductList()
+        }).catch((err) => {
+          alert(err.data.message)
+        })
+    },
+    // 取得資料
+    getProductList() {
+      const url = `${this.apiUrl}/api/${this.path}/admin/products`
+      axios.get(url)
+        .then((res) => {
+          this.productsList = res.data.products
+        })
+        .catch((err) => {
+          alert(err.data.message)
+          
+          window.location.href="./index.html"
+        })
+    },
+    // 登出
+    LogOut() {
+      const urlLogout = `${this.apiUrl}/logout`;
+      const logoutConfirm = confirm('確定要登出嗎?')
+      if (logoutConfirm) {
+        axios.post(urlLogout)
+          .then((res) => {
+            alert('成功登出，跳轉回登入頁面')
+            window.location.replace('index.html')
+          }).catch((err) => {
+            alert('登出失敗')
+          })
+      }
+    },
+  },
+  mounted() {
+    // 取得token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)backstageCookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common['Authorization'] = token;
+    this.checkLogin()
+    // add&edit modal
+    selectModal = new bootstrap.Modal(document.getElementById('selectModal'), { backdrop: 'static', keyboard: false });
+    // delete modal
+    deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'), { backdrop: 'static', keyboard: false });
+    //successModal
+    successModal = new bootstrap.Modal(document.getElementById('successModal'));
+    // errorModal
+    errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+    //批量刪除
+    deletebatchModal = new bootstrap.Modal(document.getElementById('deletebatchModal'));
+  },
+})
+app.mount('#app')
